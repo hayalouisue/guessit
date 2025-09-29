be85d8e2ddb089b750b269a880b8a83f1f69bd38
hayalouisue <haya.louisangelo@ue.edu.ph>
2025-09-29 11:14:43 +0800

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,26 @@
+// data.js
+const gameData = {
+  Test: [
+    { img: "https://cdn.pixabay.com/photo/2017/11/09/21/41/cat-2934720_640.jpg", answer: "Cat" },
+    { img: "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_640.jpg", answer: "Dog" },
+    { img: "https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706_640.jpg", answer: "Lion" }
+  ],
+  Athletes: [
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/200127110655-kobe-bryant-4-profile-shot.jpg?q=w_3000,h_2010,x_0,y_0,c_fill", answer: "Kobe Bryant" },
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/ap24029748332390.jpg?c=original", answer: "Patrick Mahomes" },
+    { img: "https://thedailyrecord.com/wp-content/blogs.dir/1/files/2023/02/Gervonta-Davis.jpg", answer: "Gervonta Davis" },
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/230621042149-01-cristiano-ronaldo-euro-200-apps-062023-restricted.jpg?c=original", answer: "Cristiano Ronaldo" },
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/200119010627-bpt112-mcgregor-cerrone-ufc-01192020.jpg?q=x_4,y_125,h_1928,w_3426,c_crop/w_1280", answer: "Conor Mcgregor" },
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/181112094027-tiger-woods-begay-tease.jpg?q=x_3,y_164,h_1682,w_2989,c_crop/w_1280", answer: "Tiger Woods" },
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/220614140938-serena-williams-wimbledon.jpg?c=original", answer: "Serena Williams"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/180916002916-02-canelo-alvarez-gennady-golovkin-fight-0915.jpg?q=x_5,y_154,h_2496,w_4435,c_crop/w_1280", answer: "Canelo Alvarez"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/2024-04-10t054204z-1645757524-mt1usatoday22988403-rtrmadp-3-nba-golden-state-warriors-at-los-angeles-lakers.JPG?c=16x9&q=h_833,w_1480,c_fill", answer: "Stephen Curry"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/230531095956-01-usain-bolt-file-2016-rio.jpg?c=original", answer: "Usian Bolt"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/180816125542-michael-phelps-tease-3.jpg?q=w_2523,h_1682,x_0,y_0,c_fill", answer: "Michael Phelps"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/221208164147-argentina-lionel-messi.jpg?c=original", answer:"Lionel Messi"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2118637638.jpg?c=original", answer: "Kawhi Leonard"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/221029113210-01-kyrie-irving-102622-restricted.jpg?c=original", answer: "Kyrie Irving"},
+    { img: "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2212134395.jpg?c=16x9&q=h_833,w_1480,c_fill", answer: "Shai Gilgeous Alexander"},
+  ]
+};
+//{ img: "", answer: "" },
